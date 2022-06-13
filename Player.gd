ab796300e2c50b8b2825f9629d9b60220d94be8c
Jay Simmons <79724024+JustIccy@users.noreply.github.com>
2022-06-13 15:03:48 -0500

    Improved Player Physics

--- Player.gd
+++ Player.gd
@@ -1,16 +1,77 @@
 extends KinematicBody
 
+#Delcare Variables
+var speed = 10
+var h_acceleration = 6
+var air_acceleration = 1
+var normal_acceleration = 6
+var gravity = 20
+var jump = 10
+var full_contact = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var mouse_sensitivity = 0.03
 
-# Called when the node enters the scene tree for the first time.
+var direction = Vector3()
+var h_velocity = Vector3()
+var movement = Vector3()
+var gravity_vec = Vector3()
+
+onready var head = $Head
+onready var ground_check = $GroundCheck
+
+#on game start keep mouse within window bounds
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+	
+#capture mouse movement and clamps the head to specific bounds on the x axis
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+
+#Physics function: Deals with player movement and speed
+func _physics_process(delta):
+	
+	direction = Vector3()
+	
+	#Calculate to check if player is either on the ground or jumping, as well as calculating for slopes and acceleration
+	if ground_check.is_colliding():
+		full_contact = true
+	
+	if not is_on_floor():
+		gravity_vec += Vector3.DOWN * gravity * delta
+		h_acceleration = air_acceleration
+	elif is_on_floor() and full_contact:
+		gravity_vec = -get_floor_normal() * gravity
+		h_acceleration = normal_acceleration
+	else:
+		gravity_vec = -get_floor_normal()
+		h_acceleration = normal_acceleration
+		
+	#Tracks Player controls
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or ground_check.is_colliding()):
+		gravity_vec = Vector3.UP * jump
+	
+	if Input.is_action_pressed("forward"):
+		direction -= transform.basis.z
+	elif Input.is_action_pressed("backward"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("left"):
+		direction -= transform.basis.x
+	elif Input.is_action_pressed("right"):
+		direction += transform.basis.x
+		
+	direction = direction.normalized()
+	h_velocity = h_velocity.linear_interpolate(direction * speed, h_acceleration * delta)
+	movement.z = h_velocity.z + gravity_vec.z
+	movement.x = h_velocity.x + gravity_vec.x
+	
+	move_and_slide(movement, Vector3.UP)
+		
+		
+		
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
