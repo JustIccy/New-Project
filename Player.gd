7c6954a878a17eed787a5f36bca000bedbed7e11
Jay Simmons <79724024+JustIccy@users.noreply.github.com>
2022-06-14 10:27:46 -0500

    update

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody
 
 #Delcare Variables
+const SWAY = 40
+const VSWAY = 50
+
 var speed = 10
 var h_acceleration = 3
 var air_acceleration = 1
@@ -8,7 +11,6 @@ var normal_acceleration = 6
 var gravity = 20
 var jump = 10
 var full_contact = false
-
 
 var mouse_sensitivity = 0.03
 
@@ -20,11 +22,14 @@ var gravity_vec = Vector3()
 onready var head = $Head
 onready var ground_check = $GroundCheck
 onready var anim_play = $Head/Camera/AnimationPlayer
+onready var hand = $Head/Hand
+onready var handlock = $Head/HandLock
 
 
 #on game start keep mouse within window bounds
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+	hand.set_as_toplevel(true)
 	
 #capture mouse movement and clamps the head to specific bounds on the x axis
 func _input(event):
@@ -32,6 +37,11 @@ func _input(event):
 		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
 		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+
+func _process(delta):
+	hand.global_transform.origin = handlock.global_transform.origin
+	hand.rotation.y = lerp_angle(hand.rotation.y, rotation.y, SWAY * delta)
+	hand.rotation.x = lerp_angle(hand.rotation.x, head.rotation.x, VSWAY * delta)
 
 #Physics function: Deals with player movement and speed
 func _physics_process(delta):
@@ -41,12 +51,13 @@ func _physics_process(delta):
 	#Calculate to check if player is either on the ground or jumping, as well as calculating for slopes and acceleration
 	if ground_check.is_colliding():
 		full_contact = true
+	else:
+		full_contact = false
 	
 	if not is_on_floor():
 		gravity_vec += Vector3.DOWN * gravity * delta
 		h_acceleration = air_acceleration
 		anim_play.stop()
-		
 	elif is_on_floor() and full_contact:
 		gravity_vec = -get_floor_normal() * gravity
 		h_acceleration = normal_acceleration
