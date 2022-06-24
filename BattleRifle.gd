f81f8797077f2a2e8607782fe1ff3195ecbe0a5d
Jay Simmons <79724024+JustIccy@users.noreply.github.com>
2022-06-24 16:20:04 -0500

    Revert "Animation, and sounds implemented"
    This reverts commit 44f730370ba74e6e093aeefaea12bb26309ebb2f.

--- BattleRifle.gd
+++ BattleRifle.gd
@@ -1,25 +1,16 @@
 extends MeshInstance
 
 var damage = 17
-var bullet_delay = 0.8
-var is_equipped = false
-onready var BR_Anim = $BR_Anim
-onready var BR_Sound = $BR_Sound
-onready var ROF = $ROF
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
 # Called when the node enters the scene tree for the first time.
-func _ready():	
-	
+func _ready():
 	pass # Replace with function body.
 
-func _process(delta):
-	if Input.is_action_just_pressed("fire") && ROF.is_stopped() && is_equipped == true:
-		BR_Anim.play("BR_anim")
-		if BR_Anim.is_playing():
-			ROF.start()
-			BR_Sound.play()
-		
 
-
-
-func _on_Timer_timeout():
-	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
