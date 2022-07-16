1abdb360914561120889dec5227a486f6368a467
Jay Simmons <79724024+JustIccy@users.noreply.github.com>
2022-07-16 10:53:59 -0500

    Moar changes

--- Player.gd
+++ Player.gd
@@ -5,8 +5,7 @@ const SWAY = 40
 const VSWAY =40
 
 var full_contact = false
-var weapon = [0]
-var is_proj = false
+
 
 var speed = 12
 const ACCEL_DEFAULT = 7
@@ -26,15 +25,13 @@ var movement = Vector3()
 
 
 onready var head = $Head
+onready var rect = $Head/Camera/TextureRect
 onready var cam = $Head/Camera
 onready var ground_check = $GroundCheck
-onready var RL = $Head/Camera/RL_CL4PP3R
-onready var bullet = preload("res://Bullet.tscn")
 onready var aimcast = $Head/Camera/Aimcast
-onready var MG = $Head/Camera/Magnum
-onready var blank = $Head/Camera/Blank
 onready var GunCam = $Head/Camera/ViewportContainer/Viewport/GunCam
-onready var muzzle = $Head/Camera/RL_CL4PP3R/Muzzle
+onready var weapon_manager = $Head/Camera/weapons
+
 
 
 
@@ -49,48 +46,14 @@ func _input(event):
 		rotate_y(deg2rad(-event.relative.x * mouse_sense))
 		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
-		
-func weapon_select():
 	
-	if Input.is_action_just_pressed("No weapons"):
-		weapon[0] = 0 
-	elif Input.is_action_just_pressed("weapon1"):	
-		weapon[0] = 1
-	elif Input.is_action_just_pressed("weapon4"):
-		weapon[0] = 4
-	
-
-	
-	if weapon[0] == 0:
-		blank.visible = true
-	else:
-		blank.visible = false
-	if weapon[0] == 1:
-		MG.visible = true
-	else:
-		MG.visible = false
-	if weapon[0] == 4:
-		is_proj = true
-		RL.visible = true
-	else:
-		is_proj = false
-		RL.visible = false
-		
-	
-		
-		
 func _process(delta):
 	#firing weapon
 	if Input.is_action_just_pressed("fire") && aimcast.is_colliding():
-		if aimcast.is_colliding():
-			if is_proj == false:
-				var target = aimcast.get_collider()
-				if target.is_in_group("Enemy"):
-					match weapon:
-						0:
-							target.health -= 0
-						1: 
-							target.health -= MG.damage
+			var target = aimcast.get_collider()
+			if target.is_in_group("Enemy"):
+				print("hit_target")
+				
 	
 	if Engine.get_frames_per_second() > Engine.iterations_per_second:
 		cam.set_as_toplevel(true)
@@ -102,26 +65,15 @@ func _process(delta):
 		cam.global_transform = head.global_transform
 
 	GunCam.global_transform = cam.global_transform
+
 #Physics function: Deals with player movement and speed
 func _physics_process(delta):
-	
-	direction = Vector3()
-	
-	weapon_select()
-	
-	#Calculate to check if player is either on the ground or jumping, as well as calculating for slopes and acceleration
-	if ground_check.is_colliding():
-		full_contact = true
-	else:
-		full_contact = false
-	
-	direction = Vector3.ZERO
-	var h_rot = global_transform.basis.get_euler().y
-	var f_input = Input.get_action_strength("backward") - Input.get_action_strength("forward")
-	var h_input = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
-	
-	#jumping and gravity
+	process_weapons()
+	process_jump(delta)
+	process_movement(delta)
+
+func process_jump(delta):
+#jumping and gravity
 	if is_on_floor():
 		snap = -get_floor_normal()
 		accel = ACCEL_DEFAULT
@@ -138,15 +90,31 @@ func _physics_process(delta):
 	#make it move
 	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
 	movement = velocity + gravity_vec
+		
+func process_movement(delta):
+	
+	direction = Vector3()
+	
+	
+	#Calculate to check if player is either on the ground or jumping, as well as calculating for slopes and acceleration
+	if ground_check.is_colliding():
+		full_contact = true
+	else:
+		full_contact = false
+	
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("backward") - Input.get_action_strength("forward")
+	var h_input = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	
+	
 	
 	move_and_slide_with_snap(movement, snap, Vector3.UP)
 	
-	#Plays headbobbing animation
-	#if direction != Vector3():
-	#	anim_play.play("Headbob")
-		
-		
-		
-
-
+func process_weapons():
+	if Input.is_action_just_pressed("No weapons"):
+		weapon_manager.change_weapon("Empty")
+	if Input.is_action_just_pressed("weapon1"):
+		weapon_manager.change_weapon("Primary")
 
--- Pulse_Lighting.gd
+++ Pulse_Lighting.gd
@@ -1,9 +1,7 @@
 extends MeshInstance
 
-var damage = 17
-var is_equipped = false
-onready var ROF = $ROF
-onready var BR_Anim = $BR_Anim
+onready var lightPulse = $OmniLight/AnimationPlayer
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,11 +10,8 @@ onready var BR_Anim = $BR_Anim
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 func _process(delta):
-	if Input.is_action_just_pressed("fire") && ROF.is_stopped() && is_equipped == true:
-		if BR_Anim.is_playing():
-			ROF.start()
+		lightPulse.play("New Anim")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
